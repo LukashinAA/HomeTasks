3f05859153a70d58ef94215fc0b5c3e68d2d975d
tema <lukashintema29@gmail.com>
2025-11-08 10:57:50 +0300

    delete

--- task3/deps.mk
+++
@@ -1,3 +0,0 @@
-listofwords.o: listofwords.c listofwords.h
-scanner.o: scanner.c scanner.h listofwords.h
-main.o: main.c scanner.h listofwords.h
--- task3/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "scanner.h"
-
-int main(void) {
-    process_input();
-    return 0;
-}
