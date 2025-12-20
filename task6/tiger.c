ce7881d0ebb87a9aac7d1628d13b910bbe2595b1
tema <lukashintema29@gmail.com>
2025-12-20 16:10:41 +0300

    task6

--- task6/tiger.c
+++ task6/tiger.c
@@ -11,9 +11,9 @@
 int shm_id;
 int sem_id;
 int *bowl;
-int *tiger_last;
-int *keeper_last;
 pid_t tiger_pid, keeper_pid;
+
+#define MAX_MEAT 10
 
 void kill_children() {
     if (tiger_pid > 0) kill(tiger_pid, SIGTERM);
@@ -23,16 +23,12 @@ void kill_children() {
 
 void cleanup() {
     kill_children();
-    if (bowl != (int*)-1 && bowl != NULL) shmdt(bowl);
+    shmdt(bowl);
     shmctl(shm_id, IPC_RMID, NULL);
     semctl(sem_id, 0, IPC_RMID);
 }
 
 void sigint_handler(int sig) {
-    exit(0);
-}
-
-void sigterm_handler(int sig) {
     exit(0);
 }
 
@@ -55,22 +51,13 @@ void tiger() {
         sem_wait();
         
         if (*bowl > 0) {
-            int eat;
-            
-            if (*tiger_last == 0) {
-                eat = rand() % 3 + 1;
-            } else {
-                eat = *tiger_last + (rand() % 3 + 1);
-            }
-            
+            int eat = rand() % 3 + 1;
             if (eat > *bowl) eat = *bowl;
             
             *bowl -= eat;
-            *tiger_last = eat;
-            
-            printf("Тигр съел %d кг. Осталось: %d кг\n", eat, *bowl);
+            printf("🐯 Тигр съел %d кг. Осталось: %d кг\n", eat, *bowl);
         } else {
-            printf("Миска пуста\n");
+            printf("🐯 Миска пуста\n");
         }
         
         sem_signal();
@@ -81,35 +68,15 @@ void keeper() {
     signal(SIGINT, SIG_IGN);
     
     while(1) {
-        sleep(rand() % 2 + 1);
+        sleep(2);
         
         sem_wait();
         
         if (*bowl == 0) {
-            int add;
-            
-            if (*keeper_last == 0) {
-                add = 10;
-            } else {
-                add = *keeper_last - 1;
-            }
-            
-            if (add < 1) add = 1;
-            
-            if (add < *tiger_last) {
-                printf("Кормление бессмысленно! Тигр ест %d кг, а можно добавить только %d кг\n", 
-                       *tiger_last, add);
-                sem_signal();
-                kill(getppid(), SIGTERM);
-                exit(0);
-            }
-            
-            *bowl = add;
-            *keeper_last = add;
-            
-            printf("Добавлено %d кг\n", add);
+            *bowl = MAX_MEAT;
+            printf("👨‍🍳 Смотритель наполнил миску: %d кг\n", *bowl);
         } else {
-            printf("В миске %d кг\n", *bowl);
+            printf("👨‍🍳 Смотритель проверил: %d кг в миске\n", *bowl);
         }
         
         sem_signal();
@@ -117,29 +84,18 @@ void keeper() {
 }
 
 int main() {
-    bowl = NULL;
-    tiger_last = NULL;
-    keeper_last = NULL;
-    tiger_pid = 0;
-    keeper_pid = 0;
-    
     signal(SIGINT, sigint_handler);
-    signal(SIGTERM, sigterm_handler);
     atexit(cleanup);
     srand(time(NULL));
     
-    shm_id = shmget(IPC_PRIVATE, 3 * sizeof(int), 0666 | IPC_CREAT);
-    bowl = (int*)shmat(shm_id, NULL, 0);
-    
-    tiger_last = bowl + 1;
-    keeper_last = bowl + 2;
-    
-    *bowl = 10;
-    *tiger_last = 0;
-    *keeper_last = 0;
+    shm_id = shmget(IPC_PRIVATE, sizeof(int), 0666 | IPC_CREAT);
+    bowl = shmat(shm_id, NULL, 0);
+    *bowl = MAX_MEAT;
+    printf("Миска создана. Начало: %d кг\n", *bowl);
     
     sem_id = semget(IPC_PRIVATE, 1, 0666 | IPC_CREAT);
     semctl(sem_id, 0, SETVAL, 1);
+    printf("Семафор создан\n");
     
     tiger_pid = fork();
     if (tiger_pid == 0) {
@@ -153,9 +109,10 @@ int main() {
         exit(0);
     }
     
-    while(1) {
-        pause();
-    }
+    printf("Для выхода: Ctrl+C\n");
+    
+    wait(NULL);
+    wait(NULL);
     
     return 0;
 }
