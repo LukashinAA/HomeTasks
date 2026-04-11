70e7f3144072a1a4f4593f03a503aa3fe3e486dc
tema <lukashintema29@gmail.com>
2026-04-11 08:36:43 +0300

    task3

---
+++ prac4/task3/StringList.h
@@ -0,0 +1,23 @@
+#ifndef STRINGLIST_H
+#define STRINGLIST_H
+
+#include "String.h"
+#include "List.h"
+
+
+class StringList : public String, public List<std::string> {
+public:
+    StringList(const char* initial = "");
+    ~StringList() override;
+
+    // contains ищет: равен ИЛИ содержит подстроку
+    bool contains(const std::string& value) const override;
+    
+    size_t length() const override;
+    std::string getType() const override;
+    void print() const;
+    
+    using List<std::string>::push_back;
+};
+
+#endif
